66ea6c8a0084d7c70cfb106c9a45deceb11c4edd
Azamat-Sult <asultangareev@gmail.com>
2021-08-22 13:42:08 +0500

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        Character[] sphere = {'\\', '|', '/', '-'};
+        int index = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\rLoading... " + sphere[index]);
+                if (index < sphere.length - 1) {
+                    index++;
+                } else {
+                    index = 0;
+                }
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException e) {
+            //e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(4000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        progress.interrupt();
+    }
+}
