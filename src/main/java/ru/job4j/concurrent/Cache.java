07bbcd805387a9bf3f2f96a14ce60264f241d1e9
Azamat-Sult <asultangareev@gmail.com>
2021-08-23 23:48:37 +0500

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/ru/job4j/concurrent/Cache.java
@@ -0,0 +1,12 @@
+package ru.job4j.concurrent;
+
+public class Cache {
+    private static Cache cache;
+
+    public synchronized static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
