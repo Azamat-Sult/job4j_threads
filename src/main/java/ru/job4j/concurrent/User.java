129f1a20012483498ae58b6bd6946a4738a16e61
Azamat-Sult <asultangareev@gmail.com>
2021-08-24 21:09:00 +0500

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/concurrent/User.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/job4j/concurrent/UserCache.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> rsl = new ArrayList<>();
+        for (User user : users.values()) {
+            rsl.add(User.of(user.getName()));
+        }
+        return rsl;
+    }
+}
