7f1d03c29fdd760ec85428b01fa4a8ec20c8827a
Azamat-Sult <asultangareev@gmail.com>
2021-08-24 19:09:43 +0500

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/concurrent/DCLSingleton.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+
+}
