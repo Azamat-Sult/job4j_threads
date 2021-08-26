adc0c85921e1eb7db7cae1bb06b67f3278911fe7
Azamat-Sult <asultangareev@gmail.com>
2021-08-26 11:41:42 +0500

    2. JCIP. Настройка библиотеки [#268575]

---
+++ src/main/java/ru/job4j/concurrent/jcip/Count.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent.jcip;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+}
