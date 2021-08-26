98d5a979f194e97342048ab4db6cd3c411b947ec
Azamat-Sult <asultangareev@gmail.com>
2021-08-26 21:09:47 +0500

    4. ThreadSafe динамический список [#1105]

---
+++ src/main/java/ru/job4j/concurrent/synch/SingleLockList.java
@@ -0,0 +1,35 @@
+package ru.job4j.concurrent.synch;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.*;
+
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
+    private final List<T> list;
+
+    public SingleLockList(List<T> list) {
+        this.list = Collections.synchronizedList(list);
+    }
+
+    public synchronized void add(T value) {
+        list.add(value);
+    }
+
+    public synchronized T get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return copy(this.list).iterator();
+    }
+
+    private List<T> copy(List<T> list) {
+        List<T> copy = new ArrayList<>();
+        copy.addAll(list);
+        return copy;
+    }
+}
---
+++ src/test/java/ru/job4j/concurrent/synch/SingleLockListTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent.synch;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.junit.Assert.*;
+
+public class SingleLockListTest {
+
+    @Test
+    public void add() throws InterruptedException {
+        List<Integer> listInt = new ArrayList<>();
+        SingleLockList<Integer> list = new SingleLockList<>(listInt);
+        Thread first = new Thread(() -> list.add(1));
+        Thread second = new Thread(() -> list.add(2));
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Set<Integer> rsl = new TreeSet<>();
+        list.iterator().forEachRemaining(rsl::add);
+        assertEquals(rsl, Set.of(1, 2));
+    }
+}
