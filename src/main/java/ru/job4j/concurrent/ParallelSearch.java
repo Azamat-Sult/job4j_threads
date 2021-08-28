d02df0f01bb5fddff7d5856979551db12548d6ca
Azamat-Sult <asultangareev@gmail.com>
2021-08-28 23:18:45 +0500

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/concurrent/ParallelSearch.java
@@ -0,0 +1,39 @@
+package ru.job4j.concurrent;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.interrupted()) {
+                        queue.poll();
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    System.out.println("Consumer stopped");
+                },
+                "Consumer"
+        );
+        consumer.start();
+
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Producer stopped");
+                    consumer.interrupt();
+                },
+                "Producer"
+
+        ).start();
+    }
+}
